9376bd9fd036eec92dd04eb72d2f724eb5291e92
appypaw <siment3187@gmail.com>
2023-07-01 22:52:20 +0900

    outputVariables, assignMultipleValues 추가

---
+++ Basic Syntax/assignMulitpleValues.py
@@ -0,0 +1,19 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#Orange
+#Banana
+#Cherry
+
+#Unpacking. 리스트나 튜플로 만들었을 때 다음과 같이 언패킹 가능.
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#Orange
+#Banana
+#Cherry
---
+++ Basic Syntax/outputVariables.py
@@ -0,0 +1,32 @@
+x = "Python is awesome"
+print(x)
+#Python is awesome
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x,y,z)
+#Python  is  awesome
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+#Python is awesome
+
+x = 5
+y = 10
+print(x + y)
+#15
+
+'''
+x = 5
+y = "John"
+print(x + y)
+'''
+#이건 안됨. 왜? 형태가 다름. x는 정수, y는 문자열.
+
+x = 5
+y = "John"
+print(x, y)
+#5 John
